08200d6193cc27f5a08102141f4493d5b92c815a
lv19 <lv19.luisvelasco@gmail.com>
2020-03-19 15:09:14 +0100

    RetrofitApp with Interceptor

--- RetrofitApp/app/src/main/java/com/example/retrofitapp/MainActivity.kt
+++ RetrofitApp/app/src/main/java/com/example/retrofitapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.retrofitapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -11,28 +14,24 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-data class Repo(val id: Long, val name: String)
-
-interface GitHubService {
-    @GET("users/{user}/repos")
-    fun listRepos(@Path("user") user : String) : Call<List<Repo>>
-}
-
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-        val service = retrofit.create(GitHubService::class.java)
+        val userRequest = UserRequest("lv19")
 
-        val serviceListRepos = service.listRepos("lv19")
+        userRequest.callListRepos().enqueue(object : Callback<List<Repo>> {
+            override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
+                Log.d(TAG, t?.message)
+            }
+            override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
+                response.body()?.forEach { Log.d(TAG, "repo:$it") }
+            }
+        })
 
-        serviceListRepos.enqueue(object : Callback<List<Repo>> {
+        userRequest.callListRepos().enqueue(object : Callback<List<Repo>> {
             override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
                 Log.d(TAG, t?.message)
             }
@@ -44,5 +43,36 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG = "MainActivity"
+    }
+}
+
+data class Repo(val id: Long, val name: String)
+
+interface GitHubService {
+    @GET("users/{user}/repos")
+    fun listRepos(@Path("user") user : String) : Call<List<Repo>>
+}
+
+class UserRequest constructor(val name: String) {
+    private val okHttpClient: OkHttpClient = OkHttpClient().newBuilder().addInterceptor { chain ->
+        val request = chain.request()
+        Log.d(TAG, "okHttpClient url=${request.url()}")
+        chain.proceed(request)
+    }.build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+    private val service = retrofit.create(GitHubService::class.java)
+
+    fun callListRepos() : Call<List<Repo>> {
+        Log.d(TAG, "name=$name")
+        return service.listRepos(name)
+    }
+
+    companion object {
+        private const val TAG = "UserRequest"
     }
 }
