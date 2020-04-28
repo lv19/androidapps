36d59fdb8834189d3bacbb56eb1ddfa57b381c16
lv19 <lv19.luisvelasco@gmail.com>
2020-04-28 14:10:41 +0200

    KRoom database with account and friends

--- KRoom/app/src/main/java/com/example/android/kroom/MainActivity.kt
+++ KRoom/app/src/main/java/com/example/android/kroom/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.room.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_layout.view.*
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -24,65 +23,114 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userAdapter = UserAdapter()
-        recyclerView.adapter = userAdapter
+        val accountAdapter = AccountAdapter()
+        recyclerView.adapter = accountAdapter
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
 
-        val userViewModel: UserViewModel by viewModels()
+        recyclerView.visibility = View.VISIBLE
+        progressBar.visibility = View.GONE
+
+        val accountViewModel: AccountViewModel by viewModels()
+        val accountFriends = accountViewModel.getAccountFriends(1)
+        accountFriends.observe(this, Observer {
+            it?.let {
+                Log.i("MainActivity", "account=${it.account} friends=${it.friends}")
+                accountAdapter.setFriends(it.friends)
+            }
+        })
+
+//        val friends1 = listOf(Friend(1, 1, "A1F1x"), Friend(3, 1, "A1F3x"))
+//        val accountFriends1 = AccountFriends(Account(1, "AccountUnoxx"), friends1)
+//        val friends2 = listOf(Friend(1, 2, "A2F1"), Friend(2, 2, "A2F2x"))
+//        val accountFriends2 = AccountFriends(Account(2, "AccountDos"), friends2)
+//        accountViewModel.insert(accountFriends1)
+//        accountViewModel.insert(accountFriends2)
         //userViewModel.insert(User(1, "Uno"))
         //userViewModel.insert(User(2, "Dos"))
-        userViewModel.users.observe(this, Observer {
-            it.forEach{user ->Log.i("MainActivity", "user=$user")}
-            recyclerView.visibility = View.VISIBLE
-            progressBar.visibility = View.GONE
-            userAdapter.setUsers(it)
-        })
+//        accountViewModel.users.observe(this, Observer {
+//            it.forEach{user ->Log.i("MainActivity", "user=$user")}
+//            recyclerView.visibility = View.VISIBLE
+//            progressBar.visibility = View.GONE
+//            accountAdapter.setFriends(it)
+//        })
     }
 }
 
-class UserAdapter(): RecyclerView.Adapter<UserAdapter.ViewHolder>(){
-    private var users = emptyList<User>()
+class AccountAdapter(): RecyclerView.Adapter<AccountAdapter.ViewHolder>(){
+    private var friends = emptyList<Friend>()
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder =
         ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false))
 
-    override fun getItemCount(): Int = users.size
+    override fun getItemCount(): Int = friends.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView.textViewName.text = users[position].name
+        holder.itemView.textViewName.text = friends[position].name
     }
 
-    fun setUsers(users: List<User>) {
-        this.users = users
+    fun setFriends(friends: List<Friend>) {
+        this.friends = friends
         notifyDataSetChanged()
     }
 }
 
+
 @Entity
-data class User(@PrimaryKey val id: Long, val name: String)
+data class Account(@PrimaryKey val id: Long, val name: String)
+
+@Entity(primaryKeys = ["id", "accountId"], foreignKeys = [
+    ForeignKey(entity = Account::class,
+        parentColumns = ["id"],
+        childColumns = ["accountId"],
+        onDelete = ForeignKey.CASCADE
+    )
+])
+data class Friend(val id: Long, val accountId: Long, val name: String)
+
+data class AccountFriends(
+    @Embedded var account: Account,
+    @Relation(parentColumn = "id", entityColumn = "accountId")
+    var friends: List<Friend> = emptyList()
+)
+
 
 @Dao
-interface UserDao {
-    @Query("select * from user")
-    fun getUsers(): LiveData<List<User>>
+interface AccountDao {
+    @Query("select * from account")
+    fun getAccounts(): LiveData<List<Account>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(user: User)
+    suspend fun insert(account: Account)
+
+    @Insert
+    suspend fun insert(friends: List<Friend>)
+
+    @Transaction
+    @Query("select * from account where account.id = :id")
+    fun getAccountFriends(id: Long): LiveData<AccountFriends>
+
+    @Transaction
+    suspend fun insert(accountFriends: AccountFriends){
+        insert(accountFriends.account)
+        insert(accountFriends.friends)
+    }
 }
 
-class UserRepository(context: Context) {
-    private val userDao: UserDao = KRoomDatabase.getInstance(context).userDao()
+class AccountRepository(context: Context) {
+    private val accountDao: AccountDao = KRoomDatabase.getInstance(context).accountDao()
 
-    val users = userDao.getUsers()
+    val accounts = accountDao.getAccounts()
 
-    suspend fun insert(user: User) = userDao.insert(user)
+    fun getAccountFriends(id: Long) = accountDao.getAccountFriends(id)
+
+    suspend fun insert(accountFriends: AccountFriends) = accountDao.insert(accountFriends)
 }
 
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [Account::class, Friend::class], version = 1, exportSchema = false)
 public abstract class KRoomDatabase: RoomDatabase() {
-    abstract fun userDao(): UserDao
+    abstract fun accountDao(): AccountDao
 
     companion object {
         private lateinit var instance: KRoomDatabase
@@ -98,12 +146,14 @@ public abstract class KRoomDatabase: RoomDatabase() {
     }
 }
 
-class UserViewModel(application: Application): AndroidViewModel(application) {
-    private val userRepository: UserRepository = UserRepository(application)
+class AccountViewModel(application: Application): AndroidViewModel(application) {
+    private val accountRepository: AccountRepository = AccountRepository(application)
 
-    val users = userRepository.users
+    val accounts = accountRepository.accounts
 
-    fun insert(user: User) = viewModelScope.launch {
-        userRepository.insert(user)
+    fun getAccountFriends(id: Long) = accountRepository.getAccountFriends(id)
+
+    fun insert(accountFriends: AccountFriends) = viewModelScope.launch {
+        accountRepository.insert(accountFriends)
     }
 }
